6eeb0859d9d22edb022b97650aca5dd8bc08daa2
mainalito <81359755+mainalito@users.noreply.github.com>
2022-06-05 21:26:42 +0300

    house listing for ndovu heights

---
+++ src/main/java/com/ndovuheights/ndovuheights/NdovuheightsApplication.java
@@ -0,0 +1,13 @@
+package com.ndovuheights.ndovuheights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdovuheightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdovuheightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndovuheights/ndovuheights/NdovuheightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndovuheights.ndovuheights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdovuheightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
