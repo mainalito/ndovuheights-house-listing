7f5319301de7dbfe610cfdfb29aa9009bec97012
mainalito <81359755+mainalito@users.noreply.github.com>
2022-06-08 18:38:38 +0300

    Rest Api business logic

--- src/main/java/com/ndovuheights/ndovuheights/Service/HouseService.java
+++ src/main/java/com/ndovuheights/ndovuheights/Service/HouseService.java
@@ -3,7 +3,6 @@ package com.ndovuheights.ndovuheights.Service;
 import java.util.List;
 import java.util.Optional;
 
-import javax.naming.NameNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,30 +13,51 @@ import com.ndovuheights.ndovuheights.model.House;
 @Service
 public class HouseService implements HouseMethods {
 
-    @Autowired
-    private HouseRepository houseRepository;
-    @Override
-    public void saveRoom(House house) {
-    
-        houseRepository.save(house);
-        
-    }
+  @Autowired
+  private HouseRepository houseRepository;
 
-    @Override
-    public List<House> fetchAllRooms() {
-         return houseRepository.findAll();
-        
-    }
+  @Override
+  public House saveRoom(House house) {
 
-    @Override
-    public Optional<House> fetchOneRoom(String roomNumber) {
-      return houseRepository.findById(roomNumber);
-        
-    }
+    return houseRepository.save(house);
 
-    @Override
-    public void deleteRoom(String roomNumber) {
-         houseRepository.deleteById(roomNumber);
+  }
+
+  @Override
+  public List<House> fetchAllRooms() {
+    return houseRepository.findAll();
+
+  }
+
+  @Override
+  public Optional<House> fetchOneRoom(String roomNumber) {
+    return houseRepository.findById(roomNumber);
+
+  }
+
+  @Override
+  public void deleteRoom(String roomNumber) {
+    houseRepository.deleteById(roomNumber);
+  }
+
+  @Override
+  public List<House> availableHouses() {
+    return houseRepository.findByStatus(true);
+  }
+
+  @Override
+  public House updateRoom(String roomNumber, House houseDetails)  {
+    Optional<House> optionalHouse = houseRepository.findById(roomNumber);
+    if(optionalHouse.isPresent()){
+     House house = optionalHouse.get();
+     house.setPrice(houseDetails.getPrice());
+     house.setRoomNumber(houseDetails.getRoomNumber());
+     house.setRoomType(houseDetails.getRoomType());
+     house.setStatus(houseDetails.isStatus());
+     saveRoom(house);
+     return house;
     }
-    
+    return null;
+  }
+
 }
